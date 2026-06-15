5e2a470357691fc08cf28914eb377dcce46ef841
26922-Golf <26922@bbw.ac.th>
2026-06-15 09:56:31 +0700

    Add files via upload
    ได้ทบทวนบทเรียนเก่าอย่างคำ else หรือ if

---
+++ 07Golf_15-6-69.py
@@ -0,0 +1,7 @@
+number = int(input("Enter your number: "))
+
+if number %12 == 0:
+    print ("Even number")
+else:
+    print ("Odd number")
+print("จัดทำโดย กิตติกร ปะวะบุตร 4/4 เลขที่7 \nขอบคุณครับ")
